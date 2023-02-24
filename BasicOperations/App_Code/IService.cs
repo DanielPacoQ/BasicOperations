7dd1b1bcbce70efbad55c72db03513161646b992
DanielPacoQ <danielpacoq@gmail.com>
2023-02-24 14:34:38 -0700

    Agregar archivos de proyecto.

---
+++ BasicOperations/App_Code/IService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+    int sum(int x, int y);
+
+    [OperationContract]
+    int Subtract(int x, int y);
+
+    [OperationContract]
+    int Multiply(int x, int y);
+
+    [OperationContract]
+    int Divide(int x, int y);
+
+    // TODO: agregue aquí sus operaciones de servicio
+}
+
+// Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+[DataContract]
+public class CompositeType
+{
+	bool boolValue = true;
+	string stringValue = "Hello ";
+
+	[DataMember]
+	public bool BoolValue
+	{
+		get { return boolValue; }
+		set { boolValue = value; }
+	}
+
+	[DataMember]
+	public string StringValue
+	{
+		get { return stringValue; }
+		set { stringValue = value; }
+	}
+}
